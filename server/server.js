d87f1bd169091e0de9904987d45525aaccf0a06e
serjay <serhiinote@gmail.com>
2025-12-18 15:49:51 +0000

    Init Backend Setup

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT);
